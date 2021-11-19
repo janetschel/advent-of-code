996136e6d051915157dcf0809fc2caf1b03fbe4f
Jan Etschel <etschelj@gmail.com>
2021-11-19 14:40:01 +0100

    day 1 for testing

--- src/main/java/dev/janetschel/calendar/day01/Puzzle.java
+++ src/main/java/dev/janetschel/calendar/day01/Puzzle.java
@@ -2,17 +2,27 @@ package dev.janetschel.calendar.day01;
 
 import java.util.List;
 
+import static dev.janetschel.utils.Converter.longs;
 import static dev.janetschel.utils.File.read;
 
 public class Puzzle {
     public static void main(String[] args) {
-        var input = read("DAY_HERE");
+        var input = read("01");
         var result = new Puzzle().solve(input);
 
         System.out.println("result = " + result);
     }
 
-    public String solve(List<String> input) {
+    public Long solve(List<String> in) {
+        var input = longs(in);
+
+        for (var x : input) {
+            for (var y : input) {
+                if (x + y == 2020) {
+                    return x * y;
+                }
+            }
+        }
 
         return null;
     }
