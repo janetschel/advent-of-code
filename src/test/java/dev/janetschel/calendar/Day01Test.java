0af0aa8c979be984992309a4c3e48a2b782f6597
Jan Etschel <etschelj@gmail.com>
2021-12-01 14:25:28 +0100

    Makefile

--- src/test/java/dev/janetschel/calendar/Day01Test.java
+++ src/test/java/dev/janetschel/calendar/Day01Test.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static dev.janetschel.utils.Converter.longs;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class Day1Test {
+public class Day01Test {
     private static GenericPuzzle puzzle;
 
     @BeforeAll
