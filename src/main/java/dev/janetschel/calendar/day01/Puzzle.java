3f4d086880b80ce603071600fecf4d780d9e4286
Jan Etschel <etschelj@gmail.com>
2021-12-01 06:15:50 +0100

    finished day 1

--- src/main/java/dev/janetschel/calendar/day01/Puzzle.java
+++ src/main/java/dev/janetschel/calendar/day01/Puzzle.java
@@ -7,18 +7,18 @@ import static dev.janetschel.utils.File.read;
 
 public class Puzzle {
     public static void main(String[] args) {
-        var input = read("01");
-        var result = new Puzzle().solvePart2(input);
+        var input = longs(read("01"));
+        var result = new Puzzle().solve(input);
+        var resultPt2 = new Puzzle().solvePart2(input);
 
         System.out.println("result = " + result);
+        System.out.println("resultPt2 = " + resultPt2);
     }
 
-    public Long solve(List<String> in) {
-        var input = longs(in);
-
-        Long increased = 0L;
-        for (int i = 1; i < input.size(); i++) {
-            if (input.get(i) > input.get(i - 1)) {
+    public Long solve(List<Long> in) {
+        var increased = 0L;
+        for (int i = 1; i < in.size(); i++) {
+            if (in.get(i) > in.get(i - 1)) {
                 increased++;
             }
         }
@@ -26,13 +26,11 @@ public class Puzzle {
         return increased;
     }
 
-    public Long solvePart2(List<String> in) {
-        var input = longs(in);
-
+    public Long solvePart2(List<Long> in) {
         var prev = 0L;
         var increased = 0L;
-        for (int i = 0; i < input.size() - 3; i++) {
-            var sum = input.get(i) + input.get(i + 1) + input.get(i + 2);
+        for (int i = 0; i < in.size() - 3; i++) {
+            var sum = in.get(i) + in.get(i + 1) + in.get(i + 2);
             if (sum > prev) {
                 increased++;
             }
