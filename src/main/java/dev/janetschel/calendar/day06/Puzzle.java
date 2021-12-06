f6565d0816fffbaa740580025c8cad1d6f84d510
Jan Etschel <etschelj@gmail.com>
2021-12-06 06:36:26 +0100

    day 6 finished

---
+++ src/main/java/dev/janetschel/calendar/day06/Puzzle.java
@@ -0,0 +1,57 @@
+package dev.janetschel.calendar.day06;
+
+import dev.janetschel.interfaces.GenericPuzzle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.LongStream;
+
+import static dev.janetschel.util.Converter.longs;
+import static java.lang.Integer.parseInt;
+
+public class Puzzle implements GenericPuzzle {
+    // Could've refactored like in part1 to make it run faster (pt1: ~86ms, pt2: ~4ms) but nah
+    public Long solvePart1(List<String> input) {
+        var longs = longs(Arrays.stream(input.get(0).split(",")).toList());
+        var current = new ArrayList<>(longs);
+
+        for (int i = 0; i < 80; i++) {
+            for (int j = 0; j < current.size(); j++) {
+                if (current.get(j) == 0) {
+                    current.set(j, 6L);
+                    current.add(9L);
+                } else {
+                    current.set(j, current.get(j) - 1);
+                }
+            }
+
+        }
+
+        return (long) current.size();
+    }
+
+    public Long solvePart2(List<String> input) {
+        long[] counts = new long[9];
+
+        for (var line : input.get(0).split(",")) {
+            ++counts[parseInt(line)];
+        }
+
+        for (long i = 0; i < 256; i++) {
+            long[] newCounts = new long[9];
+
+            //noinspection ManualArrayCopy
+            for (int j = 1; j < newCounts.length; j++) {
+                newCounts[j - 1] = counts[j];
+            }
+
+            newCounts[6] += counts[0];
+            newCounts[8] += counts[0];
+
+            counts = newCounts;
+        }
+
+        return LongStream.of(counts).sum();
+    }
+}
---
+++ src/main/java/dev/janetschel/calendar/day06/README.md
@@ -0,0 +1,7 @@
+# Solve times
+
+- `00:10:39` for part 1 (#3364) ⭐️
+- `00:16:45` for part 2 (#1472) ⭐️
+
+Δ time from part 1 to part 2: `+00:06:06`  
+Δ places from part 1 to part 2: `-1892`
