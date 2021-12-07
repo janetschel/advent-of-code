2f9f7e877395cb278cca7898a47aab129d69b188
Jan Etschel <etschelj@gmail.com>
2021-12-07 16:50:16 +0100

    refactor

--- src/main/java/dev/janetschel/util/meta/Request.java
+++ src/main/java/dev/janetschel/util/meta/Request.java
@@ -7,13 +7,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+import static dev.janetschel.util.meta.File.readFile;
 import static java.lang.Integer.parseInt;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 
 class Request {
     private static final String REQUEST_URL = "https://adventofcode.com";
-    private static String SESSION_KEY = null;
+    private static final String SESSION_KEY = retrieveSessionKey();
 
     static String fetchInput(String year, String day) throws IOException {
         day = sanitizeDayForRequest(day);
@@ -21,27 +22,20 @@ class Request {
         var url = new URL("%s/%s/day/%s/input".formatted(REQUEST_URL, year, day));
         var connection = url.openConnection();
 
-        retrieveSessionKey();
         connection.setRequestProperty("Cookie", "session=%s".formatted(SESSION_KEY));
 
         return CharStreams.toString(new InputStreamReader(connection.getInputStream(), UTF_8));
     }
 
     @SneakyThrows
-    private static void retrieveSessionKey() {
-        if (SESSION_KEY != null) {
-            // SESSION_KEY has already been read, no need to do it again
-            return;
-        }
-
-        SESSION_KEY = File.readFile("src/main/resources/secret.yml")
+    private static String retrieveSessionKey() {
+        return readFile("src/main/resources/secret.yml")
                 .split(": ")[1]
                 .replaceAll("\"", "")
                 .trim();
     }
 
     private static String sanitizeDayForRequest(String day) {
-        // Hack to remove leading 0 in days
         return String.valueOf(parseInt(day));
     }
 }
